9ea62e84d3181bb720c0d6479d535d4c6d011ac7
Ömer YILDIRIM <yildirimomer01@gmail.com>
2021-04-21 23:03:47 +0300

    pages added

---
+++ app/src/main/java/com/yildirimomer01/popitv/ui/detailing/DetailFragment.kt
@@ -0,0 +1,9 @@
+package com.yildirimomer01.popitv.ui.detailing
+
+import androidx.fragment.app.Fragment
+import com.yildirimomer01.popitv.R
+
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+}
---
+++ app/src/main/java/com/yildirimomer01/popitv/ui/listing/ListFragment.kt
@@ -0,0 +1,9 @@
+package com.yildirimomer01.popitv.ui.listing
+
+import androidx.fragment.app.Fragment
+import com.yildirimomer01.popitv.R
+
+
+class ListFragment : Fragment(R.layout.fragment_list) {
+
+}
