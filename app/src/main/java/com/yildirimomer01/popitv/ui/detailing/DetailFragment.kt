54ab520a261fa367be90dc47419239eaba4e5167
Ömer YILDIRIM <yildirimomer01@gmail.com>
2021-04-21 23:24:33 +0300

    app nav added

--- app/src/main/java/com/yildirimomer01/popitv/ui/detailing/DetailFragment.kt
+++ app/src/main/java/com/yildirimomer01/popitv/ui/detailing/DetailFragment.kt
@@ -2,8 +2,9 @@ package com.yildirimomer01.popitv.ui.detailing
 
 import androidx.fragment.app.Fragment
 import com.yildirimomer01.popitv.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
 
 }
--- app/src/main/java/com/yildirimomer01/popitv/ui/listing/ListFragment.kt
+++ app/src/main/java/com/yildirimomer01/popitv/ui/listing/ListFragment.kt
@@ -2,8 +2,9 @@ package com.yildirimomer01.popitv.ui.listing
 
 import androidx.fragment.app.Fragment
 import com.yildirimomer01.popitv.R
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class ListFragment : Fragment(R.layout.fragment_list) {
 
 }
