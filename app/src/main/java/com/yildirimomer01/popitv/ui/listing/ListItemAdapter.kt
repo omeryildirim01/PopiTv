d38c5b7bf1a1238411e88c5a4fb40ed11a8b184f
Ömer YILDIRIM <yildirimomer01@gmail.com>
2021-04-24 03:48:56 +0300

    list item ui data binding

--- app/src/main/java/com/yildirimomer01/popitv/ui/listing/ListItemAdapter.kt
+++ app/src/main/java/com/yildirimomer01/popitv/ui/listing/ListItemAdapter.kt
@@ -3,11 +3,14 @@ package com.yildirimomer01.popitv.ui.listing
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.yildirimomer01.popitv.R
+import com.yildirimomer01.popitv.databinding.ItemCreatorBinding
+import com.yildirimomer01.popitv.databinding.ListItemBinding
 import com.yildirimomer01.popitv.module.GlideApp
 import com.yildirimomer01.popitv.model.TvShow
 import kotlinx.android.synthetic.main.list_item.view.*
@@ -15,45 +18,27 @@ import timber.log.Timber
 
 class ListItemAdapter : PagingDataAdapter<TvShow, ListItemAdapter.ItemViewHolder>(COMPARATOR) {
 
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    class ItemViewHolder(itemBinding:ListItemBinding) : RecyclerView.ViewHolder(itemBinding.root){
+        var bindingItem:  ListItemBinding = itemBinding
         companion object{
-            fun from(parent: ViewGroup):ItemViewHolder{
-
-                try {
-                    val inflater = LayoutInflater.from(parent.context)
-                    val itemView = inflater.inflate(R.layout.list_item,parent,false)
-                    return ItemViewHolder(itemView = itemView)
-                }catch (ex:Throwable){
-                    Timber.e(ex)
-                    return ItemViewHolder(itemView = View(parent.context))
-                }
+            fun from(itemBinding:ListItemBinding):ItemViewHolder{
+                return ItemViewHolder(itemBinding)
             }
         }
         fun bind(tvShow:TvShow){
-            itemView.setOnClickListener{
+            bindingItem.tvshow = tvShow
+            bindingItem.executePendingBindings();
+
+            bindingItem.cardView.setOnClickListener{
                 val directions= ListFragmentDirections.actionListFragmentToDetailFragment(tvShow.id!! )
                 it.findNavController().navigate(directions)
             }
-            itemView.apply {
-                GlideApp.with(ivPoster)
-                    .load("https://image.tmdb.org/t/p/w500${tvShow.posterPath}")
-                    .placeholder(R.drawable.ic_image_placeholder)
-                    .error(R.drawable.ic_error_image)
-                    .into(ivPoster)
-                tVTitle.text = tvShow.originalName
-                tVReleaseDate.text = tvShow.firstAirDate
-                tvShow?.voteAverage?.let { rating->
-                    rbRating.rating = (rating/2).toFloat()
-                }
-                tvVoteCount.text = tvShow?.voteCount.toString()
-            }
-
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder.from(parent)
+        val binding : ListItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.context),R.layout.list_item, parent, false)
+        return ItemViewHolder.from(binding)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
